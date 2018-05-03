754670256a14ce036086d726c45019aa2df92f2e
Travis CI User <travis@example.org>
2018-05-03 15:13:24 +0000

    Documentation for rust-lang-nursery/rand@aaaf03f

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rand/jitter/enum.TimerError.html\" title=\"enum rand::jitter::TimerError\">TimerError</a>",synthetic:false,types:["rand::jitter::TimerError"]},];
+implementors["rand_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rand_core/enum.ErrorKind.html\" title=\"enum rand_core::ErrorKind\">ErrorKind</a>",synthetic:false,types:["rand_core::error::ErrorKind"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rand/read/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ReadRng","An RNG that reads random bytes straight from a `Read`."]]});
